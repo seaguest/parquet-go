512e0f5df54adf4b4ebc74e2b05331f1f965cebc
Achille Roussel <achille@segment.com>
2022-02-10 03:57:23 +0000

    optimize WriteInt16x8/WriteInt32x8/WriteInt64x8

--- internal/bits/writer.go
+++ internal/bits/writer.go
@@ -39,15 +39,17 @@ func (w *Writer) WriteInt16x8(data [][8]int16, bitWidth uint) error {
 	if bitWidth > 16 {
 		return fmt.Errorf("cannot write 16 bits values to %d bits", bitWidth)
 	}
-	buf := (*[64]byte)(w.buffer[:])
+	buf := &w.buffer
 
-	for i := range data {
-		*buf = [64]byte{}
-		writeInt16x8(buf, &data[i], bitWidth)
+	for i := 0; i < len(data); {
+		n := writeInt16x8(buf, data[i:len(data):len(data)], bitWidth)
+		k := uint(n) * bitWidth
 
-		if _, err := w.writer.Write(buf[:bitWidth]); err != nil {
+		if _, err := w.writer.Write(buf[:k]); err != nil {
 			return err
 		}
+
+		i += n
 	}
 
 	return nil
@@ -57,15 +59,17 @@ func (w *Writer) WriteInt32x8(data [][8]int32, bitWidth uint) error {
 	if bitWidth > 32 {
 		return fmt.Errorf("cannot write 32 bits values to %d bits", bitWidth)
 	}
-	buf := (*[64]byte)(w.buffer[:])
+	buf := &w.buffer
 
-	for i := range data {
-		*buf = [64]byte{}
-		writeInt32x8(buf, &data[i], bitWidth)
+	for i := 0; i < len(data); {
+		n := writeInt32x8(buf, data[i:len(data):len(data)], bitWidth)
+		k := uint(n) * bitWidth
 
-		if _, err := w.writer.Write(buf[:bitWidth]); err != nil {
+		if _, err := w.writer.Write(buf[:k]); err != nil {
 			return err
 		}
+
+		i += n
 	}
 
 	return nil
@@ -75,15 +79,17 @@ func (w *Writer) WriteInt64x8(data [][8]int64, bitWidth uint) error {
 	if bitWidth > 64 {
 		return fmt.Errorf("cannot write 64 bits values to %d bits", bitWidth)
 	}
-	buf := (*[64]byte)(w.buffer[:])
+	buf := &w.buffer
 
-	for i := range data {
-		*buf = [64]byte{}
-		writeInt64x8(buf, &data[i], bitWidth)
+	for i := 0; i < len(data); {
+		n := writeInt64x8(buf, data[i:len(data):len(data)], bitWidth)
+		k := uint(n) * bitWidth
 
-		if _, err := w.writer.Write(buf[:bitWidth]); err != nil {
+		if _, err := w.writer.Write(buf[:k]); err != nil {
 			return err
 		}
+
+		i += n
 	}
 
 	return nil
@@ -92,7 +98,6 @@ func (w *Writer) WriteInt64x8(data [][8]int64, bitWidth uint) error {
 func writeInt8x8(dst *[512]byte, src [][8]int8, bitWidth uint) int {
 	m := uint64(1<<bitWidth) - 1
 	n := 512 / bitWidth
-
 	if uint(len(src)) > n {
 		src = src[:n:n]
 	}
@@ -117,96 +122,92 @@ func writeInt8x8(dst *[512]byte, src [][8]int8, bitWidth uint) int {
 	return len(src)
 }
 
-func writeInt16x8(dst *[64]byte, src *[8]int16, bitWidth uint) {
-	mask := uint64(1<<bitWidth) - 1
-	bits := [8]uint64{
-		0: uint64(src[0]) & mask,
-		1: uint64(src[1]) & mask,
-		2: uint64(src[2]) & mask,
-		3: uint64(src[3]) & mask,
-		4: uint64(src[4]) & mask,
-		5: uint64(src[5]) & mask,
-		6: uint64(src[6]) & mask,
-		7: uint64(src[7]) & mask,
+func writeInt16x8(dst *[512]byte, src [][8]int16, bitWidth uint) int {
+	m := int16(1<<bitWidth) - 1
+	n := 512 / bitWidth
+	if uint(len(src)) > n {
+		src = src[:n:n]
 	}
-	writeUint64x8(dst, &bits, bitWidth)
-}
 
-func writeInt32x8(dst *[64]byte, src *[8]int32, bitWidth uint) {
-	mask := uint64(1<<bitWidth) - 1
-	bits := [8]uint64{
-		0: uint64(src[0]) & mask,
-		1: uint64(src[1]) & mask,
-		2: uint64(src[2]) & mask,
-		3: uint64(src[3]) & mask,
-		4: uint64(src[4]) & mask,
-		5: uint64(src[5]) & mask,
-		6: uint64(src[6]) & mask,
-		7: uint64(src[7]) & mask,
+	b := unsafe.Slice(*(**int16)(unsafe.Pointer(&dst)), len(dst)/2)
+	for i := range b {
+		b[i] = 0
 	}
-	writeUint64x8(dst, &bits, bitWidth)
-}
+	_ = b[:(bitWidth*uint(len(src)))/2]
 
-func writeInt64x8(dst *[64]byte, src *[8]int64, bitWidth uint) {
-	mask := uint64(1<<bitWidth) - 1
-	bits := [8]uint64{
-		0: uint64(src[0]) & mask,
-		1: uint64(src[1]) & mask,
-		2: uint64(src[2]) & mask,
-		3: uint64(src[3]) & mask,
-		4: uint64(src[4]) & mask,
-		5: uint64(src[5]) & mask,
-		6: uint64(src[6]) & mask,
-		7: uint64(src[7]) & mask,
-	}
-	writeUint64x8(dst, &bits, bitWidth)
-}
-
-func writeUint64x8(dst *[64]byte, src *[8]uint64, bitWidth uint) {
-	b := (*[8]uint64)(unsafe.Pointer(dst))
-	c := uint64(0)
-	i := uint(0)
-	j := uint(0)
 	k := uint(0)
-
-	i, j = k/64, k%64
-	b[i] |= src[0] << j
-
-	k += bitWidth
-	i, j = k/64, k%64
-	b[i] |= src[1] << j
-
-	c = src[1] >> (64 - j)
-	k += bitWidth
-	i, j = k/64, k%64
-	b[i] |= (src[2] << j) | c
-
-	c = src[2] >> (64 - j)
-	k += bitWidth
-	i, j = k/64, k%64
-	b[i] |= (src[3] << j) | c
-
-	c = src[3] >> (64 - j)
-	k += bitWidth
-	i, j = k/64, k%64
-	b[i] |= (src[4] << j) | c
-
-	c = src[4] >> (64 - j)
-	k += bitWidth
-	i, j = k/64, k%64
-	b[i] |= (src[5] << j) | c
-
-	c = src[5] >> (64 - j)
-	k += bitWidth
-	i, j = k/64, k%64
-	b[i] |= (src[6] << j) | c
-
-	c = src[6] >> (64 - j)
-	k += bitWidth
-	i, j = k/64, k%64
-	b[i] |= (src[7] << j) | c
-
-	if c = src[7] >> (64 - j); c != 0 {
-		b[(k+bitWidth)/64] |= c
+	c := int16(0)
+	for i := range src {
+		for _, v := range src[i] {
+			q, r := k/16, k%16
+			bits := v & m
+			b[q] |= (bits << r) | c
+			c = bits >> (16 - r)
+			k += bitWidth
+		}
 	}
+	if c != 0 {
+		b[k/16] |= c
+	}
+	return len(src)
+}
+
+func writeInt32x8(dst *[512]byte, src [][8]int32, bitWidth uint) int {
+	m := int32(1<<bitWidth) - 1
+	n := 512 / bitWidth
+	if uint(len(src)) > n {
+		src = src[:n:n]
+	}
+
+	b := unsafe.Slice(*(**int32)(unsafe.Pointer(&dst)), len(dst)/4)
+	for i := range b {
+		b[i] = 0
+	}
+	_ = b[:(bitWidth*uint(len(src)))/4]
+
+	k := uint(0)
+	c := int32(0)
+	for i := range src {
+		for _, v := range src[i] {
+			q, r := k/32, k%32
+			bits := v & m
+			b[q] |= (bits << r) | c
+			c = bits >> (32 - r)
+			k += bitWidth
+		}
+	}
+	if c != 0 {
+		b[k/32] |= c
+	}
+	return len(src)
+}
+
+func writeInt64x8(dst *[512]byte, src [][8]int64, bitWidth uint) int {
+	m := int64(1<<bitWidth) - 1
+	n := 512 / bitWidth
+	if uint(len(src)) > n {
+		src = src[:n:n]
+	}
+
+	b := unsafe.Slice(*(**int64)(unsafe.Pointer(&dst)), len(dst)/8)
+	for i := range b {
+		b[i] = 0
+	}
+	_ = b[:(bitWidth*uint(len(src)))/8]
+
+	k := uint(0)
+	c := int64(0)
+	for i := range src {
+		for _, v := range src[i] {
+			q, r := k/64, k%64
+			bits := v & m
+			b[q] |= (bits << r) | c
+			c = bits >> (64 - r)
+			k += bitWidth
+		}
+	}
+	if c != 0 {
+		b[k/64] |= c
+	}
+	return len(src)
 }
